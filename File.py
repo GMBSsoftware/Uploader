ea9558fe18e36a6a7829513e16206ce8bb381276
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2024-09-21 00:06:50 +0900

    설정 UI 완료.

--- File.py
+++ File.py
@@ -2,26 +2,23 @@ import os
 
 
 class File:
-    def __init__(self, file_name, location) -> None:
+    def __init__(self, file_name, path) -> None:
         self.file_name = file_name
-        self.file_location = location
-        if os.path.isfile(self.file_location):
-            self.file_size = self.get_file_size(
-                self.file_location
-            )  # 파일 사이즈 초기화
+        self.file_path = path
+        full_path = os.path.join(self.file_path, self.file_name)  # 전체 파일 경로 생성
+
+        if os.path.isfile(full_path):  # 전체 경로로 파일 존재 여부 확인
+            self.file_size = self.get_file_size(full_path)  # 파일 사이즈 초기화
         else:
-            print(f"파일이 존재하지 않습니다: {self.file_location}")
+            print(f"파일이 존재하지 않습니다: {full_path}")
             self.file_size = 0  # 파일이 존재하지 않을 경우 사이즈를 0으로 설정
 
-    def moveTo(self, file, location_to_move):
+    def moveTo(self, file, path_to_move):
         pass
 
-    def get_file_size(self, file):
-        try:
-            return os.path.getsize(file)
-        except Exception as e:
-            print(f"파일 사이즈를 가져오는 중 오류 발생: {e}")
-            return 0
+    def get_file_size(self, full_path):
+        """파일 크기를 바이트 단위로 반환하는 함수"""
+        return os.path.getsize(full_path)
 
     def check_file_size_under_max_size(self, file, max_size) -> bool:
         if isinstance(file, File):
@@ -30,4 +27,4 @@ class File:
             raise TypeError("잘못된 인스턴스")
 
     def __str__(self):
-        return f"파일명: {self.file_name}, 위치: {self.file_location}, 크기: {self.file_size} 바이트"
+        return f"파일명: {self.file_name}, 위치: {self.file_path}, 크기: {self.file_size} 바이트"
--- GUI.py
+++ GUI.py
@@ -4,9 +4,11 @@ from File import File
 from tkinter import messagebox
 from tkinterdnd2 import TkinterDnD, DND_FILES
 
+from Setting import Setting
+
 
 class GUI:
-    def __init__(self, root) -> None:
+    def __init__(self, root, setting) -> None:
         self.version = "Ver1.0"
         self.root = root
         # 생성할 프로그램 가로 세로 크기
@@ -15,6 +17,9 @@ class GUI:
         # 현재 화면을 나타내는 프레임
         self.current_frame = None
         self.file_list = []
+        self.setting = setting
+        self.entry_widgets = []  # 엔트리 위젯(설정 값)을 저장할 리스트 추가
+        self.frame_base()
 
     def frame_base(self):
         self.root.title("Uploader")
@@ -48,9 +53,7 @@ class GUI:
         self.frame_width = self.app_width
         self.frame_height = self.app_height - 60
 
-    def frame_home(self):
-        self.frame_remove()
-
+    def create_frame(self):
         # 프레임 생성
         self.current_frame = tk.Frame(self.root)
         self.current_frame.pack(fill="both", expand=True)
@@ -58,24 +61,39 @@ class GUI:
             x=0, y=0, width=self.frame_width, height=self.frame_height
         )
 
-    def frame_home_and_setting(self):
+    def page_home(self):
+        self.frame_remove()
+        self.create_frame()
+        self.button_change_page("home")
+        self.frame_drag_and_drop()
+
+    def page_setting(self):
+        self.frame_remove()
+        self.clear_widgets()
+        self.create_frame()
+        self.button_change_page("setting")
+        self.frame_setting()
+
+    def button_change_page(self, current_page):
+        """홈이면 설정 버튼, 설정 페이지면 홈"""
+
+        text = "설정" if current_page == "home" else "홈"
+        command = (
+            self.on_setting_click if current_page == "home" else self.on_home_click
+        )
+
         # 프레임 생성
         self.current_frame = tk.Frame(self.root)
         self.current_frame.place(x=0, y=0, width=self.frame_width, height=80)
 
-        button_home = tk.Button(
-            self.current_frame, text="홈", command=self.on_home_click, relief="groove"
-        )
-        button_home.place(x=self.frame_width - 40 - 35, y=35, width=40, height=40)
-
-        button_setting = tk.Button(
+        button_change_page = tk.Button(
             self.current_frame,
-            text="설정",
-            command=self.on_setting_click,
+            text=text,
+            command=command,
             relief="groove",
         )
-        button_setting.place(
-            x=self.frame_width - 40 - 35 - 70, y=35, width=40, height=40
+        button_change_page.place(
+            x=self.frame_width - 40 - 35, y=35, width=40, height=40
         )
 
     def frame_remove(self):
@@ -84,28 +102,73 @@ class GUI:
             self.current_frame.destroy()
 
     def on_home_click(self):
-        self.frame_home()
+        self.save_settings()
+        self.page_home()
 
     def on_setting_click(self):
-        messagebox.showinfo("알림", "업데이트 예정")
+        self.page_setting()
 
     def show(self):
-        self.frame_base()
-        self.frame_home_and_setting()
-        self.frame_drag_and_drop()
+        self.page_home()
 
     def frame_drag_and_drop(self):
         # 파일 드래그 앤 드롭을 위한 리스트박스
         self.listbox = tk.Listbox(self.root, width=60, height=10)
-        self.listbox.pack(pady=20)
+        self.listbox.place(x=50, y=100)
 
         # 드래그 앤 드롭 기능을 리스트박스에 추가
         self.listbox.drop_target_register(DND_FILES)
         self.listbox.dnd_bind("<<Drop>>", self.on_file_drop)
 
-        # 실행 버튼 추가
-        self.execute_button = tk.Button(self.root, text="실행", command=self.on_execute)
-        self.execute_button.pack(pady=10)
+        # 업로드 버튼
+        self.upload_button = tk.Button(
+            self.root, text="업로드", command=self.on_upload, relief="groove"
+        )
+        self.upload_button.place(x=520, y=100, width=70, height=70)
+
+        # 목록 지우기 버튼
+        self.delete_button = tk.Button(
+            self.root, text="목록\n지우기", command=self.on_delete, relief="groove"
+        )
+        self.delete_button.place(x=520, y=200, width=70, height=70)
+
+    def frame_setting(self):
+        # 설정 프레임 내의 라벨 추가
+        label = tk.Label(self.root, text="설정 값", font=30)
+        label.place(x=70, y=30)
+
+        # 엔트리 위젯 추가를 위한 데이터
+        entries = [
+            ("광명 홈페이지 아이디", self.setting.id_gm),
+            ("광명 홈페이지 비밀번호", self.setting.password_gm),
+            ("네이버 아이디", self.setting.id_naver),
+            ("네이버 비밀번호", self.setting.password_naver),
+            ("메일 받는 사람", self.setting.receive_email),
+            ("저장할 나스 위치", self.setting.nas_path),
+        ]
+
+        for i, (label_text, default_value) in enumerate(entries):
+            # 레이블 추가
+            entry_label = tk.Label(self.root, text=label_text)
+            entry_label.place(x=50, y=90 + (i * 30), anchor="nw")  # y 좌표 조정
+
+            # 엔트리 추가
+            entry = tk.Entry(self.root, width=40, justify="left")
+            entry.place(x=200, y=90 + (i * 30))  # y 좌표 조정
+            entry.insert(0, default_value)  # 기본값 설정
+            self.entry_widgets.append(entry)  # 엔트리 위젯 저장
+
+    def save_settings(self):
+        """엔트리 값을 setting에 저장하는 함수"""
+        self.setting.id_gm = self.entry_widgets[0].get()
+        self.setting.password_gm = self.entry_widgets[1].get()
+        self.setting.id_naver = self.entry_widgets[2].get()
+        self.setting.password_naver = self.entry_widgets[3].get()
+        self.setting.receive_email = self.entry_widgets[4].get()
+        self.setting.nas_path = self.entry_widgets[5].get()
+
+    def clear_widgets(self):
+        self.entry_widgets.clear()
 
     def on_file_drop(self, event):
         """파일 드롭 시 호출되는 함수"""
@@ -113,27 +176,32 @@ class GUI:
         for file in files:
             self.listbox.insert(tk.END, file)  # 드롭된 파일 경로 추가
 
-    def on_execute(self):
+    def on_upload(self):
         self.file_list = self.create_files_from_list(
             self.listbox.get(0, tk.END)
         )  # 리스트박스의 파일 경로로 File 객체 리스트 생성
         for file in self.file_list:
-            print(file)  # File 객체의 __str__ 메서드 호출로 파일 정보 출력
+            print("file : ", file)  # File 객체의 __str__ 메서드 호출로 파일 정보 출력
+
+    def on_delete(self):
+        """리스트박스의 모든 항목을 지우는 함수"""
+        self.listbox.delete(0, tk.END)  # 리스트박스의 모든 항목 삭제
 
     def create_files_from_list(self, file_paths):
         """파일 경로 리스트를 받아서 File 객체 리스트를 반환하는 함수"""
         file_objects = []
         for file_path in file_paths:
             file_name = os.path.basename(file_path)
-            file_location = file_path
-            file_obj = File(file_name, file_location)
+            file_path = os.path.dirname(file_path)
+            file_obj = File(file_name, file_path)
             file_objects.append(file_obj)
         return file_objects
 
 
+setting = Setting()
 if __name__ == "__main__":
     root = TkinterDnD.Tk()
-    gui = GUI(root)
+    gui = GUI(root, setting)
     gui.show()
 
     gui.root.mainloop()
